fd7b35065d2976dde5ede76434f191d764e44fa8
sampson.obenembot <sampson.obenembot@sgits.co.za>
2022-11-07 21:31:47 +0200

    web socket

---
+++ src/test/java/com/example/websocketproject/WebsocketProjectApplicationTestt.java
@@ -0,0 +1,13 @@
+package com.example.websocketproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketProjectApplicationTestt {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
